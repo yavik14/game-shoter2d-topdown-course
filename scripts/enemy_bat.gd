fc036bdd43bf897ab0ba4bfd8e796ad45f5900d1
The YaviK Team <theyavikteam@gmail.com>
2026-01-09 18:23:23 +0100

    Player and enemy damage

--- scripts/enemy_bat.gd
+++ scripts/enemy_bat.gd
@@ -1,15 +1,33 @@
 extends CharacterBody2D
 
 @export var speed = 25
+@export var health = 3
 
 @onready var player = get_node("/root/Level01/Player")
 @onready var anims = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+func _ready():
+	anims.play("fly")
+	anims.connect("animation_finished", self._on_animation_finished)
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
 	if velocity.x < 0: 
-		anims.play("flyLeft")
+		sprite.flip_h = true
 	else:
-		anims.play("flyRight")
+		sprite.flip_h = false
+		
+func take_damage():
+	anims.stop()
+	anims.play("hurt")
+	health -= 1
+	if (health <= 0):
+		queue_free()
+		
+func _on_animation_finished(anim_name):
+	if anim_name == "hurt":
+		anims.play("fly")
+		
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,51 @@
 extends CharacterBody2D
 
 @export var speed = 125
+@export var health = 100
 
 @onready var anims = $AnimationPlayer
+@onready var hurtZone = $HurtZone
+@onready var sprite = $Sprite2D
+
+var isHurt = false
+
+func _ready():
+	anims.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
 	move_and_slide()
-	
-	var targetPosion = get_global_mouse_position()
-	
-	if direction.length() > 0 :
-		if targetPosion.x < global_position.x:
-			anims.play("walkLeft")
+		
+	if (isHurt == false) :		
+		if direction.length() > 0 :
+			anims.play("walk")
 		else:
-			anims.play("walkRight")
+			anims.play("idle")
+			
+	var targetPosion = get_global_mouse_position()
+			
+	if targetPosion.x < global_position.x:
+		sprite.flip_h = true
 	else:
-		anims.play("idle")
-
+		sprite.flip_h = false
+		
+	var overlapping_enemies = hurtZone.get_overlapping_bodies()
+	for enemy in overlapping_enemies:
+		if enemy.is_in_group("Enemies"):
+			take_damage(1)
+			break
+			
+func take_damage(amount):
+	health -= amount
+	isHurt = true
+	anims.play("hurt")
+	if health <= 0:
+		die()
+		
+func die():
+	get_tree().reload_current_scene()
 	
+func _on_animation_finished(anim_name):
+	if anim_name == "hurt":
+		isHurt = false
